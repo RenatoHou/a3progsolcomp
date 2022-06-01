70d72db68fdae4d944f31336d79f9dc01d0ab83b
Renato Hou <renato.hou@gmail.com>
2022-05-31 22:51:23 -0300

    Cliente

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -1,6 +1,27 @@
 package dao;
 
+import com.mycompany.projetoa3.Connexao_SQL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.Cliente;
+
 
 public class ClienteDAO {
+    private Connection connection;
+
+    public ClienteDAO() throws SQLException {     
+        connection = Connexao_SQL.getConnection();
+    }
+    
+    public boolean insertCliente(Cliente cliente) throws SQLException{
+        String sqlQuery = "INSERT INTO cliente (cpf, nome, idade) VALUES (?,?,?)";
+        PreparedStatement statment = connection.prepareStatement(sqlQuery);
+        statment.setInt(1, cliente.getCpf());
+        statment.setString(2, cliente.getNome());
+        statment.setInt(3, cliente.getIdade());
+        int rowsInserted = statment.executeUpdate();      
+        return (rowsInserted > 0);
+    }
     
 }
--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -17,6 +17,32 @@ public class Cliente {
         this.nome = nome;
         this.idade = idade;
     }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
     
     
 }
